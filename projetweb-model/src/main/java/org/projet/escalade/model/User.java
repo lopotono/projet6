9eae258e5b8e0e472b9fbb1e7ef8ac062759c7f8
lopotono <30410435+lopotono@users.noreply.github.com>
2018-04-26 12:18:23 +0200

    ajout user.java

---
+++ projetweb-model/src/main/java/org/projet/escalade/model/User.java
@@ -0,0 +1,20 @@
+package org.projet.escalade.model;
+
+public class User {
+
+	private String username;
+	private String userprenom;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUserprenom() {
+		return userprenom;
+	}
+	public void setUserprenom(String userprenom) {
+		this.userprenom = userprenom;
+	}
+}
