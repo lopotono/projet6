b01e0e2a66f2771b14d80e58ceb8279517b962eb
lopotono <30410435+lopotono@users.noreply.github.com>
2018-04-26 12:17:37 +0200

    ajout UserDAOImpl.java

---
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/UserDAOImpl.java
@@ -0,0 +1,31 @@
+package org.projet.escalade.consumer.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.projet.escalade.consumer.contract.UserDAO;
+import org.projet.escalade.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+public class UserDAOImpl extends AbstractDAO implements UserDAO {
+	
+	public List<User> getUser() {
+		String vSQL = "SELECT * FROM utilisateur";
+		
+		JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+		
+		RowMapper<User> vRowMapper = new RowMapper<User>() {
+			public User mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+				User vUser = new User();
+				vUser.setUsername(pRS.getString("nom_user"));
+				vUser.setUserprenom(pRS.getString("prenom_user"));
+				return vUser;
+			}
+		};
+		List<User> vListUser = vJdbcTemplate.query(vSQL, vRowMapper);
+		
+		return vListUser;
+	}
+}
