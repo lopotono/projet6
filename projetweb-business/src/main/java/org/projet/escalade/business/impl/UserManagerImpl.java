cc7ae36b6025a04e8cc1d8540e262e9a06a12db1
lopotono <30410435+lopotono@users.noreply.github.com>
2018-04-26 12:15:59 +0200

    ajout UserManagerImpl.java

---
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/UserManagerImpl.java
@@ -0,0 +1,13 @@
+package org.projet.escalade.business.impl;
+
+import org.projet.escalade.business.contract.UserManager;
+import org.projet.escalade.model.User;
+
+public class UserManagerImpl extends AbstractManager implements UserManager {
+
+	public User getUser(String login, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
