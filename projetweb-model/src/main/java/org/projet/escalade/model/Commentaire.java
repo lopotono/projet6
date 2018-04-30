3e3cd54fd3b32ddd2c4b0dbb11aef7d584bb064a
lopotono <30410435+lopotono@users.noreply.github.com>
2018-04-30 17:41:31 +0200

    mise à jour module model

---
+++ projetweb-model/src/main/java/org/projet/escalade/model/Commentaire.java
@@ -0,0 +1,14 @@
+package org.projet.escalade.model;
+
+public class Commentaire {
+	
+	private String commentairename;
+
+	public String getCommentairename() {
+		return commentairename;
+	}
+
+	public void setCommentairename(String commentairename) {
+		this.commentairename = commentairename;
+	}
+}
