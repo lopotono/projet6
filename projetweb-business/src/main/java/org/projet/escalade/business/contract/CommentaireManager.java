5822e5655e5880bf299649eda5fbc611a1ef105e
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-03 14:23:53 +0200

    Mise à jour CommentaireManager.java

--- projetweb-business/src/main/java/org/projet/escalade/business/contract/CommentaireManager.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/contract/CommentaireManager.java
@@ -3,8 +3,22 @@ package org.projet.escalade.business.contract;
 import java.util.List;
 
 import org.projet.escalade.model.Commentaire;
+import org.projet.escalade.model.Message;
+import org.projet.escalade.model.exception.FunctionalException;
+import org.projet.escalade.model.exception.NotFoundException;
+import org.projet.escalade.model.exception.TechnicalException;
 
 public interface CommentaireManager {
-	
-	public List<Commentaire> getCommentaire(); 
+
+	public List<Commentaire> getCommentaire();
+
+	Commentaire getCommentaire(String pName) throws NotFoundException;
+
+	void addCommentaire(Commentaire pCommentaire) throws FunctionalException, TechnicalException;
+
+	List<Message> getListNewMessage(Commentaire pCommentaire, Integer pLastMessageindex)
+			throws NotFoundException, TechnicalException;
+
+	void addMessage(Commentaire pCommentaire, Message pMessage)
+			throws FunctionalException, NotFoundException, TechnicalException;
 }
