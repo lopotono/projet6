c3ff74c2b515434ec98d829ff80573d9b4eaed1d
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-03 14:26:02 +0200

    Mise à jour Commentaire.java

--- projetweb-model/src/main/java/org/projet/escalade/model/Commentaire.java
+++ projetweb-model/src/main/java/org/projet/escalade/model/Commentaire.java
@@ -1,14 +1,37 @@
 package org.projet.escalade.model;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Commentaire {
 	
-	private String commentairename;
+	private String name;
 
-	public String getCommentairename() {
-		return commentairename;
+	public Commentaire() {
+		
+	}
+	
+	public String getName() {
+		return name;
 	}
 
-	public void setCommentairename(String commentairename) {
-		this.commentairename = commentairename;
+	public void setname(String pName) {
+		name = pName;
+	}
+	
+	public String toString() {
+		final StringBuilder vStB = new StringBuilder(this.getClass().getSimpleName());
+		vStB.append(" {")
+			.append("name=").append(name)
+			.append("}");
+		return vStB.toString();
+	}
+	
+	public boolean equals(Object obj) {
+		return super.equals(obj)
+				|| (obj instanceof Commentaire && StringUtils.equals(((Commentaire) obj).getName(), name));
+	}
+	
+	public int hashCode() {
+		return this.name != null ? this.name.hashCode() : super.hashCode();
 	}
 }
