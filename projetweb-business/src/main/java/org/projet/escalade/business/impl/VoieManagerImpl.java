b145f0201c15dcb061b541e858d1a0d74bf5d0ff
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-09 08:42:51 +0200

    Modifications VoieManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/VoieManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/VoieManagerImpl.java
@@ -4,15 +4,10 @@ import java.util.List;
 
 import org.projet.escalade.business.contract.VoieManager;
 import org.projet.escalade.model.Voie;
-import org.projet.escalade.model.exception.NotFoundException;
 
 public class VoieManagerImpl extends AbstractManager implements VoieManager {
 
 	public List<Voie> getListVoie() {
 		return getDaoFactory().getVoieDao().getVoie();
-	}
-
-	public Voie getVoie(Integer pID) throws NotFoundException {
-		return null;
 	}
 }
