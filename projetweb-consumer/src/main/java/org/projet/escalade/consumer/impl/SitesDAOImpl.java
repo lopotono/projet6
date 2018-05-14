ccca9cf570d47138c7316793245077ade7646222
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-14 17:09:54 +0200

    Mise à jour SitesDAOImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/SitesDAOImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/SitesDAOImpl.java
@@ -1,33 +1,36 @@
 package org.projet.escalade.consumer.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.projet.escalade.consumer.contract.SitesDAO;
+import org.projet.escalade.consumer.impl.rawmapper.SiteRawMapper;
 import org.projet.escalade.model.Sites;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+
 
 public class SitesDAOImpl extends AbstractDAO implements SitesDAO {
 	
-	public List<Sites> getSites() {
-		String vSQL = "SELECT * FROM site_escalade";
-		
-		JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
-		
-		RowMapper<Sites> vRowMapper = new RowMapper<Sites>() {
-			public Sites mapRow(ResultSet pRS, int pRowNum) throws SQLException {
-				Sites vSites = new Sites();
-				vSites.setSitesid(pRS.getInt("id_site_escalade"));
-				vSites.setSitesname(pRS.getString("nom_site"));
-				return vSites;
-			}		
-		};
-		
-		List<Sites> vListSites = vJdbcTemplate.query(vSQL, vRowMapper);
-
-        return vListSites;		
+	public SitesDAOImpl() {
+		setJdbcTemplate (new JdbcTemplate(getDataSource()));
 	}
 
+	public List<Sites> getSites() {
+		String vSQL = "SELECT * FROM site_escalade";
+
+		SiteRawMapper vRowMapper = new SiteRawMapper(); 
+		
+		List<Sites> vListSites = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return vListSites;
+	}
+
+	public Sites getSite(int id) {
+		String vSQL = "SELECT * FROM site_escalade WHERE id_site_escalade="+id;
+
+		SiteRawMapper vRowMapper = new SiteRawMapper();
+
+		List<Sites> vListSites = getJdbcTemplate().query(vSQL, vRowMapper);
+
+		return vListSites.get(0);
+	}
 }
