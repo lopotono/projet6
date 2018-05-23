22622b4856ef8b1b297e1ad6f8953042a7aa73ed
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 17:46:11 +0200

    Mise à jour de VoieManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/VoieManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/VoieManagerImpl.java
@@ -3,11 +3,21 @@ package org.projet.escalade.business.impl;
 import java.util.List;
 
 import org.projet.escalade.business.contract.VoieManager;
+import org.projet.escalade.model.Secteur;
 import org.projet.escalade.model.Voie;
 
 public class VoieManagerImpl extends AbstractManager implements VoieManager {
 
 	public List<Voie> getListVoie() {
 		return getDaoFactory().getVoieDao().getVoie();
+	}
+
+	public Voie getVoie(int id) {
+		Voie vVoie = getDaoFactory().getVoieDao().getVoie(id);
+		return vVoie;		
+	}
+	
+	public List<Voie> getVoieBySecteur(Secteur vSecteur) {
+		return getDaoFactory().getVoieDao().getVoieBySecteur(vSecteur);
 	}
 }
