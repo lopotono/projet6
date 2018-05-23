2a825a010cca356039f9c5324dd4b30986be3d29
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 11:21:28 +0200

    Mise à jour de Sites.java

--- projetweb-model/src/main/java/org/projet/escalade/model/Sites.java
+++ projetweb-model/src/main/java/org/projet/escalade/model/Sites.java
@@ -1,9 +1,13 @@
 package org.projet.escalade.model;
 
+import java.util.List;
+
 public class Sites {
-	
+		
 	private String sitesname;
 	private int sitesid;
+	private List<Secteur> secteurs;
+	private List<Voie> voie;
 
     public String getSitesname() {
         return sitesname;
@@ -19,5 +23,21 @@ public class Sites {
     
 	public void setSitesid(int sitesid) {
 		this.sitesid = sitesid;		
+	}
+
+	public List<Secteur> getSecteurs() {
+		return secteurs;
+	}
+
+	public void setSecteurs(List<Secteur> secteurs) {
+		this.secteurs = secteurs;
+	}
+
+	public List<Voie> getVoie() {
+		return voie;
+	}
+
+	public void setVoie(List<Voie> voie) {
+		this.voie = voie;
 	} 	
 }
