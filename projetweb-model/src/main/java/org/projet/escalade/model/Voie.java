7fb87fe996c1f88bbc430383c36393a9dd593427
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 11:22:38 +0200

    Mise à jour de Voie.java

--- projetweb-model/src/main/java/org/projet/escalade/model/Voie.java
+++ projetweb-model/src/main/java/org/projet/escalade/model/Voie.java
@@ -2,31 +2,37 @@ package org.projet.escalade.model;
 
 public class Voie {
 	
+	private Integer id;
 	private Integer voiehauteur;
-	private Integer voienombrepoints;
 	private Integer voienumerocotation;
-		
+	private Integer voienombrepoints;
 	
-		public Integer getVoiehauteur() {
-			return voiehauteur;
-		}
+	public Voie() {
 		
-		public void setVoiehauteur(Integer voiehauteur) {
-			this.voiehauteur = voiehauteur;
-		}
-		
-		public Integer getVoienombrepoints() {
-			return voienombrepoints;
-		}
-		
-		public void setVoienombrepoints(Integer voienombrepoints) {
-			this.voienombrepoints = voienombrepoints;
-		}
-		
-		public Integer getVoienumerocotation() {
-			return voienumerocotation;
-		}
-		public void setVoienumerocotation(Integer voienumerocotation) {
-			this.voienumerocotation = voienumerocotation;
-		}		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getVoiehauteur() {
+		return voiehauteur;
+	}
+	public void setVoiehauteur(Integer voiehauteur) {
+		this.voiehauteur = voiehauteur;
+	}
+	public Integer getVoienumerocotation() {
+		return voienumerocotation;
+	}
+	public void setVoienumerocotation(Integer voienumerocotation) {
+		this.voienumerocotation = voienumerocotation;
+	}
+	public Integer getVoienombrepoints() {
+		return voienombrepoints;
+	}
+	public void setVoienombrepoints(Integer voienombrepoints) {
+		this.voienombrepoints = voienombrepoints;
+	}
 }
