403be090fe5258ca613e4ed99ca649149ef12da8
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 11:19:49 +0200

    Ajout de VoieRawMapper.java

---
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/rawmapper/VoieRawMapper.java
@@ -0,0 +1,18 @@
+package org.projet.escalade.consumer.impl.rawmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.projet.escalade.model.Voie;
+import org.springframework.jdbc.core.RowMapper;
+
+public class VoieRawMapper implements RowMapper<Voie> {
+	
+	public Voie mapRow(ResultSet pRS, int pRowNum) throws SQLException {
+		Voie vVoie = new Voie();
+		vVoie.setVoiehauteur(pRS.getInt("hauteur"));
+		vVoie.setVoienumerocotation(pRS.getInt("numero_cotation"));
+		vVoie.setVoienombrepoints(pRS.getInt("nombre_points"));	
+		return vVoie;
+	}
+}
