bbc1db2440d3403d20cacfc10773d387c0ef83df
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 11:17:09 +0200

    Mise à jour VoieDAOImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/VoieDAOImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/VoieDAOImpl.java
@@ -1,32 +1,45 @@
 package org.projet.escalade.consumer.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.projet.escalade.consumer.contract.VoieDAO;
+import org.projet.escalade.consumer.impl.rawmapper.VoieRawMapper;
+import org.projet.escalade.model.Sites;
 import org.projet.escalade.model.Voie;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
 public class VoieDAOImpl extends AbstractDAO implements VoieDAO {
 	
+	public VoieDAOImpl() {
+		
+	}
+
 	public List<Voie> getVoie() {
-		String vSQL = "SELECT * FROM voie";
+		String vSQL = "SELECT * FROM voie";		
 		
-		JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+		VoieRawMapper vRowMapper = new VoieRawMapper();
+			
+		List<Voie> vListVoie = getJdbcTemplate().query(vSQL, vRowMapper);
 		
-		RowMapper<Voie> vRowMapper = new RowMapper<Voie>() {
-            public Voie mapRow(ResultSet pRS, int pRowNum) throws SQLException {
-                Voie vVoie = new Voie();
-                vVoie.setVoiehauteur(pRS.getInt("hauteur"));
-                vVoie.setVoienumerocotation(pRS.getInt("numero_cotation"));
-                vVoie.setVoienombrepoints(pRS.getInt("nombre_points"));
-                return vVoie;
-            }
-		};
+		return vListVoie;
+	}
+
+	public Voie getVoie(int id) {
+		String vSQL = "SELECT * FROM voie WHERE id_voie="+id;
 		
-		List<Voie> vListVoie = vJdbcTemplate.query(vSQL, vRowMapper);
+		VoieRawMapper vRowMapper = new VoieRawMapper();
+		
+		List<Voie> vListVoie = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		return vListVoie.get(0);
+	}
+	
+	public List<Voie> getVoieBySite(Sites vSite) {
+		
+		String vSQL = "SELECT * FROM voie WHERE id_site_escalade="+vSite.getSitesid();
+		
+		VoieRawMapper vRowMapper = new VoieRawMapper();
+		
+		List<Voie> vListVoie = getJdbcTemplate().query(vSQL, vRowMapper);
 		
 		return vListVoie;		
 	}
