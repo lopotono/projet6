441154ca44d8f382db25a21857524069df337d5a
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 17:53:02 +0200

    Mise à jour de DaoFactoryImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/DaoFactoryImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/DaoFactoryImpl.java
@@ -1,58 +1,56 @@
 package org.projet.escalade.consumer.impl;
 
-import org.projet.escalade.consumer.contract.CommentaireDAO;
 import org.projet.escalade.consumer.contract.DaoFactory;
+import org.projet.escalade.consumer.contract.LongueurDAO;
 import org.projet.escalade.consumer.contract.SecteurDAO;
 import org.projet.escalade.consumer.contract.SitesDAO;
 import org.projet.escalade.consumer.contract.TopoDAO;
 import org.projet.escalade.consumer.contract.UserDAO;
+import org.projet.escalade.consumer.contract.VoieDAO;
 
 public class DaoFactoryImpl implements DaoFactory {
+
+	private TopoDAO topoDao;
+	private SecteurDAO secteurDao;
+	private SitesDAO sitesDao;
+	private UserDAO userDao;
+	private VoieDAO voieDao;
+	private LongueurDAO longueurDao;
 	
-    private TopoDAO topoDao;
-    private SecteurDAO secteurDao;
-    private SitesDAO sitesDao;
-    private UserDAO userDao;
-    private CommentaireDAO commentaireDAO;
- 
-    
-    public TopoDAO getTopoDao() {
-    	return topoDao;
-    }
-    
-    public void setTopoDao(TopoDAO topoDao) {
+	public TopoDAO getTopoDao() {
+		return topoDao;
+	}
+	public void setTopoDao(TopoDAO topoDao) {
 		this.topoDao = topoDao;
 	}
-
 	public SecteurDAO getSecteurDao() {
 		return secteurDao;
 	}
-	
 	public void setSecteurDao(SecteurDAO secteurDao) {
 		this.secteurDao = secteurDao;
 	}
-
 	public SitesDAO getSitesDao() {
 		return sitesDao;
 	}
-	
-	public void setSitesDAO(SitesDAO sitesDAO) {
-		this.sitesDao = sitesDAO;
+	public void setSitesDao(SitesDAO sitesDao) {
+		this.sitesDao = sitesDao;
 	}
-
 	public UserDAO getUserDao() {
 		return userDao;
 	}
-
 	public void setUserDao(UserDAO userDao) {
 		this.userDao = userDao;
 	}
-
-	public CommentaireDAO getCommentaireDao() {
-		return commentaireDAO;
+	public VoieDAO getVoieDao() {
+		return voieDao;
 	}
-	
-	public void setCommentaireDAO(CommentaireDAO commentaireDAO) {
-		this.commentaireDAO = commentaireDAO;
+	public void setVoieDao(VoieDAO voieDao) {
+		this.voieDao = voieDao;
+	}
+	public LongueurDAO getLongueurDao() {
+		return longueurDao;
+	}
+	public void setLongueurDao(LongueurDAO longueurDao) {
+		this.longueurDao = longueurDao;
 	}
 }
