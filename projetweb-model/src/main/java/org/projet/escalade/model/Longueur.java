499ff181669bda5f5c268116580fd7427c7b2819
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 17:56:48 +0200

    Ajout de Longueur.java

---
+++ projetweb-model/src/main/java/org/projet/escalade/model/Longueur.java
@@ -0,0 +1,20 @@
+package org.projet.escalade.model;
+
+public class Longueur {
+	
+	private Integer id;
+	private Integer longueurnombre;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getLongueurnombre() {
+		return longueurnombre;
+	}
+	public void setLongueurnombre(Integer longueurnombre) {
+		this.longueurnombre = longueurnombre;
+	}
+}
