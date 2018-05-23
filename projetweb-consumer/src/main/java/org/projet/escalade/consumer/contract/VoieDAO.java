c804edd61263f4db85d8cd908699117039cccb71
lopotono <30410435+lopotono@users.noreply.github.com>
2018-05-23 11:13:54 +0200

    Mise à jour VoieDAO.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/VoieDAO.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/VoieDAO.java
@@ -2,9 +2,14 @@ package org.projet.escalade.consumer.contract;
 
 import java.util.List;
 
+import org.projet.escalade.model.Sites;
 import org.projet.escalade.model.Voie;
 
 public interface VoieDAO {
 	
-	public List<Voie> getVoie();
+	List<Voie> getVoie();
+
+	Voie getVoie(int id);
+
+	List<Voie> getVoieBySite(Sites vSite);	
 }
