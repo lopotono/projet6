41246c67dda8dcb524c14e8c9dcb7093fe7b4582
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-19 07:40:12 +0200

    Mise à jour de SitesDAO.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/SitesDAO.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/SitesDAO.java
@@ -3,11 +3,16 @@ package org.projet.escalade.consumer.contract;
 import java.util.List;
 
 import org.projet.escalade.model.Sites;
+import org.projet.escalade.model.Topos;
 
 public interface SitesDAO {
 	
 	List<Sites> getSites();
 	
 	Sites getSite(int id);
+
+	List<Sites> getSiteByTopos(Topos vTopo);
+
+	List<Sites> getSearchSite(String name);
 }
 
