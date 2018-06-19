6f4cd85c415434f4068491ff311496698911be50
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-19 07:38:18 +0200

    Mise à jour de SitesManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/SitesManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/SitesManagerImpl.java
@@ -7,6 +7,7 @@ import org.projet.escalade.model.Longueur;
 import org.projet.escalade.model.Points;
 import org.projet.escalade.model.Secteur;
 import org.projet.escalade.model.Sites;
+import org.projet.escalade.model.Topos;
 import org.projet.escalade.model.Voie;
 
 public class SitesManagerImpl extends AbstractManager implements SitesManager {
@@ -19,21 +20,29 @@ public class SitesManagerImpl extends AbstractManager implements SitesManager {
 		Sites vSite = getDaoFactory().getSitesDao().getSite(id);
 		List<Secteur> vSecteurs = getDaoFactory().getSecteurDao().getSecteurBySite(vSite);
 		vSite.setSecteurs(vSecteurs);
-		
+
 		for (Secteur secteur : vSecteurs) {
 			List<Voie> vVoies = getDaoFactory().getVoieDao().getVoieBySecteur(secteur);
 			secteur.setVoie(vVoies);
-				
+
 			for (Voie voie : vVoies) {
 				List<Longueur> vLongueur = getDaoFactory().getLongueurDao().getLongueurByVoie(voie);
 				voie.setLongueur(vLongueur);
-				
+
 				for (Longueur longueur : vLongueur) {
 					List<Points> vPoints = getDaoFactory().getPointsDao().getPointsByLongueur(longueur);
 					longueur.setPoints(vPoints);
 				}
-			}			
-		}	
+			}
+		}
 		return vSite;
+	}
+
+	public List<Sites> getSiteByTopos(Topos vTopo) {
+		return getDaoFactory().getSitesDao().getSiteByTopos(vTopo);
+	}
+
+	public List<Sites> getSearchSite(String name) {
+		return getDaoFactory().getSitesDao().getSearchSite(name);
 	}
 }
