cb903dd4830adb6ec2aef03a6cb785d2cf0647a6
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-19 07:36:50 +0200

    Mise à jour de SitesManager.java

--- projetweb-business/src/main/java/org/projet/escalade/business/contract/SitesManager.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/contract/SitesManager.java
@@ -3,10 +3,15 @@ package org.projet.escalade.business.contract;
 import java.util.List;
 
 import org.projet.escalade.model.Sites;
+import org.projet.escalade.model.Topos;
 
 public interface SitesManager {
 
 	List<Sites> getListSites();
 	
 	Sites getSite(int id);
+	
+	List<Sites> getSiteByTopos(Topos vTopo);
+
+	List<Sites> getSearchSite(String name);
 }
