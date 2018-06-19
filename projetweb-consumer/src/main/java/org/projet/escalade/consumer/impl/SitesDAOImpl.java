9879cf1a4a29cf44b5291e5dd5bbd3e34ed65232
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-19 07:42:07 +0200

    Mise à jour de SitesDAOImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/SitesDAOImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/SitesDAOImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.projet.escalade.consumer.contract.SitesDAO;
 import org.projet.escalade.consumer.impl.rawmapper.SiteRawMapper;
 import org.projet.escalade.model.Sites;
+import org.projet.escalade.model.Topos;
 
 public class SitesDAOImpl extends AbstractDAO implements SitesDAO {
 	
@@ -32,5 +33,27 @@ public class SitesDAOImpl extends AbstractDAO implements SitesDAO {
 		List<Sites> vListSites = getJdbcTemplate().query(vSQL, vRowMapper);
 
 		return vListSites.get(0);
+	}
+
+	public List<Sites> getSiteByTopos(Topos vTopo) {
+		
+		String vSQL = "SELECT * FROM site_escalade WHERE id_topo=" + vTopo.getId();
+		
+		SiteRawMapper vRowMapper = new SiteRawMapper();
+		
+		List<Sites> vListSites = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		return vListSites;
+	}
+
+	public List<Sites> getSearchSite(String name) {
+		
+		String vSQL = "SELECT * FROM site_escalade WHERE nom_site LIKE '%"+ name + "%'";
+		
+		SiteRawMapper vRowMapper = new SiteRawMapper();
+		
+		List<Sites> sites = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		return sites;
 	}
 }
