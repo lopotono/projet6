657ad1cfc8ae465e28aab009f64761d01ac411a7
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-25 16:49:29 +0200

    Mise à jour LoginAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/LoginAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/LoginAction.java
@@ -1,14 +1,14 @@
 package org.projet.escalade.webapp.action;
 
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.struts2.interceptor.SessionAware;
 import org.projet.escalade.model.User;
 import org.projet.escalade.model.exception.NotFoundException;
-import org.apache.commons.lang3.StringUtils;
 
 import com.opensymphony.xwork2.ActionSupport;
-
 
 public class LoginAction extends AbstractAction implements SessionAware {
 
@@ -16,44 +16,54 @@ public class LoginAction extends AbstractAction implements SessionAware {
 	 * 
 	 */
 	private static final long serialVersionUID = -4510595769280222445L;
-	
-	private String login;
+
+	private String name;
 	private String password;
+	private Map<String, Object> session;
+
+	public String getName() {
+		return name;
+	}
 	
-	public String getLogin() {
-		return login;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
-	public String doLogin() throws NotFoundException {
+	public String doLogin() {
 		String vResult = ActionSupport.INPUT;
-		if (!StringUtils.isAllEmpty(login, password)) {
-			User vUser = AbstractAction.getManagerFactory().getUserManager().getUser(login, password);
-			
-			this.session.put("user", vUser);
-			
-			vResult = ActionSupport.SUCCESS;
+		if (!StringUtils.isAllEmpty(name, password)) {
+			try {
+				User vUser = AbstractAction.getManagerFactory().getUserManager().getUser(name, password);
+
+				if (vUser == null) {
+					throw new NotFoundException();
+				}
+				this.session.put("user", vUser);
+
+				vResult = ActionSupport.SUCCESS;
+				
+			} catch (NotFoundException pE) {
+				this.addActionError("Identifiant ou mot de passe invalide !");
+			}
 		}
 		return vResult;
 	}
-	
+
 	public String doLogout() {
-		
+
 		this.session.remove("user");
-		
+
 		return ActionSupport.SUCCESS;
 	}
-	
-	private Map<String, Object> session;
-	
+
 	public void setSession(Map<String, Object> pSession) {
 		this.session = pSession;
 	}
