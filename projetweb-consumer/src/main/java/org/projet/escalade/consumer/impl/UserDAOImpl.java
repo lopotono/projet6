a220c126e9d35c368d3f88bf68dd2fbdca7e8165
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-25 16:54:48 +0200

    Mise à jour de UserDAOImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/UserDAOImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/UserDAOImpl.java
@@ -1,48 +1,47 @@
 package org.projet.escalade.consumer.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.projet.escalade.consumer.contract.UserDAO;
+import org.projet.escalade.consumer.impl.rawmapper.UserRawMapper;
 import org.projet.escalade.model.User;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-
 
 public class UserDAOImpl extends AbstractDAO implements UserDAO {
 	
 	public List<User> getUser() {
 		
 		String vSQL = "SELECT * FROM utilisateur";		
+				
+		UserRawMapper vRowMapper = new UserRawMapper(); 
 		
-		JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
-		
-		RowMapper<User> vRowMapper = new RowMapper<User>() {
-			public User mapRow(ResultSet pRS, int pRowNum) throws SQLException {
-				User vUser = new User();
-				vUser.setUsername(pRS.getString("nom_user"));
-				vUser.setUserprenom(pRS.getString("prenom_user"));
-				vUser.setUsermail(pRS.getString("mail_user"));
-				vUser.setUseradresse(pRS.getString("adresse"));
-				vUser.setUsercodepostal(pRS.getInt("code_postal"));
-				vUser.setUserville(pRS.getString("ville"));
-				return vUser;
-			}
-		};
-		List<User> vListUser = vJdbcTemplate.query(vSQL, vRowMapper);
+		List<User> vListUser = getJdbcTemplate().query(vSQL, vRowMapper);
 		
 		return vListUser;
 	}
-
-	public void creer(User user) {
-		// TODO Auto-generated method stub
+	
+	public User getUser(int id) {
 		
+		String vSQL = "SELECT * FROM utilisateur WHERE id_user="+id;
+		
+		UserRawMapper vRowMapper = new UserRawMapper(); 
+		
+		List<User> vListUser = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		return vListUser.get(0);		
 	}
 
-	public User trouver(String username) {
-		// TODO Auto-generated method stub
-		return null;
+	public User getUser(String name, String password) {
+		
+		String vSQL = "SELECT * FROM utilisateur WHERE nom_user='"+ name + "' AND mot_de_passe='" + password + "'";
+		System.out.println(vSQL);
+		UserRawMapper vRowMapper = new UserRawMapper();
+		
+		List<User> user = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		if (user.isEmpty()) {
+			return null;
+		}
+		return user.get(0);
 	}
 }
 
