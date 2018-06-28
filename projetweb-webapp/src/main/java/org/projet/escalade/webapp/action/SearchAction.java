06c04d1bfc9709488432ecc1fd9430867e75812a
lopotono <30410435+lopotono@users.noreply.github.com>
2018-06-28 11:44:02 +0200

    Mise à jour de SearchAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/SearchAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/SearchAction.java
@@ -14,7 +14,7 @@ public class SearchAction extends AbstractAction {
 	 * 
 	 */
 	private static final long serialVersionUID = 6893326862926901279L;
-	
+
 	private List<Sites> searchsite;
 	private List<Secteur> searchsecteur;
 	private List<Voie> searchvoie;
@@ -23,41 +23,50 @@ public class SearchAction extends AbstractAction {
 	public List<Sites> getSearchsite() {
 		return searchsite;
 	}
+
 	public void setSearchsite(List<Sites> searchsite) {
 		this.searchsite = searchsite;
 	}
-	
+
 	public List<Secteur> getSearchsecteur() {
 		return searchsecteur;
 	}
+
 	public void setSearchsecteur(List<Secteur> searchsecteur) {
 		this.searchsecteur = searchsecteur;
 	}
-	
+
 	public List<Voie> getSearchvoie() {
 		return searchvoie;
 	}
+
 	public void setSearchvoie(List<Voie> searchvoie) {
 		this.searchvoie = searchvoie;
-	}	
-	
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-			
-	public String execute() {
-		
-		setSearchsite(getManagerFactory().getSitesManager().getSearchSite(name));
-		
-		setSearchsecteur(getManagerFactory().getSecteurManager().getSearchSecteur(name));
-		
-		setSearchvoie(getManagerFactory().getVoieManager().getSearchVoie(name));
-				
-		return ActionSupport.SUCCESS;
-	}
 
+	public String execute() {
+
+		if (name != null) {
+
+			setSearchsite(getManagerFactory().getSitesManager().getSearchSite(name));
+
+			setSearchsecteur(getManagerFactory().getSecteurManager().getSearchSecteur(name));
+
+			setSearchvoie(getManagerFactory().getVoieManager().getSearchVoie(name));
+
+			return ActionSupport.SUCCESS;
+			
+		} else {
+			addActionError("Aucun résultat !");
+			return ActionSupport.ERROR;
+		}
+	}
 }
