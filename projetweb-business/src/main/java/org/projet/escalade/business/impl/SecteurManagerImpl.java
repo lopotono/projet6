2c556f51af08ef0e32f04079ea90fcd2029dc2df
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-23 15:08:05 +0200

    Mise à jour de SecteurManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/SecteurManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/SecteurManagerImpl.java
@@ -6,8 +6,12 @@ import org.projet.escalade.business.contract.SecteurManager;
 import org.projet.escalade.model.Secteur;
 import org.projet.escalade.model.Sites;
 import org.projet.escalade.model.Voie;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
-public class SecteurManagerImpl extends AbstractManager implements SecteurManager{
+public class SecteurManagerImpl extends AbstractManager implements SecteurManager {
 
 	public List<Secteur> getListSecteur() {
 		return getDaoFactory().getSecteurDao().getSecteur();
@@ -16,15 +20,30 @@ public class SecteurManagerImpl extends AbstractManager implements SecteurManage
 	public List<Secteur> getSecteurBySite(Sites vSite) {
 		return getDaoFactory().getSecteurDao().getSecteurBySite(vSite);
 	}
-	
+
 	public Secteur getSecteur(int id) {
 		Secteur vSecteur = getDaoFactory().getSecteurDao().getSecteur(id);
-		List<Voie> vVoie = getDaoFactory().getVoieDao().getVoieBySecteur(vSecteur);	
+		List<Voie> vVoie = getDaoFactory().getVoieDao().getVoieBySecteur(vSecteur);
 		vSecteur.setVoie(vVoie);
 		return vSecteur;
 	}
 
 	public List<Secteur> getSearchSecteur(String name) {
 		return getDaoFactory().getSecteurDao().getSearchSecteur(name);
-	}	
+	}
+
+	@Transactional(value = "txManagerSite")
+	public void AddSecteur(final String name, final int id_site) {
+
+		TransactionTemplate vTransactionTemplate = new TransactionTemplate(getPlatformTransactionManager());
+
+		vTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+
+				getDaoFactory().getSecteurDao().AddSecteur(name, id_site);
+			};
+		});
+	}
 }
