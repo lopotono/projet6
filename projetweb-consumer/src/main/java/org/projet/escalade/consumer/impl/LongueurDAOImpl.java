2dfa6bd0999284864f4d4ca8bd8c4a932924b5b9
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-23 17:49:52 +0200

    Mise à jour de LongueurDAOImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/LongueurDAOImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/LongueurDAOImpl.java
@@ -1,11 +1,15 @@
 package org.projet.escalade.consumer.impl;
 
+import java.sql.Types;
 import java.util.List;
 
 import org.projet.escalade.consumer.contract.LongueurDAO;
 import org.projet.escalade.consumer.impl.rawmapper.LongueurRawMapper;
 import org.projet.escalade.model.Longueur;
 import org.projet.escalade.model.Voie;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class LongueurDAOImpl extends AbstractDAO implements LongueurDAO {
 	
@@ -26,7 +30,7 @@ public class LongueurDAOImpl extends AbstractDAO implements LongueurDAO {
 	public List<Longueur> getLongueurByVoie(Voie vVoie) {
 		
 		String vSQL = "SELECT * FROM longueur WHERE id_voie="+vVoie.getId();
-		System.out.println(vSQL);
+		
 		LongueurRawMapper vRowMapper = new LongueurRawMapper();
 		
 		List<Longueur> vListLongueur = getJdbcTemplate().query(vSQL, vRowMapper);
@@ -43,5 +47,23 @@ public class LongueurDAOImpl extends AbstractDAO implements LongueurDAO {
 		List<Longueur> vListLongueur = getJdbcTemplate().query(vSQL, vRowMapper);
 		
 		return vListLongueur.get(0);
+	}
+
+	public void AddLongueur(int id, int nblongueur, int id_voie) {
+		
+		String vSQL = "INSERT INTO longueur (id_longueur, nombre_longueur, id_voie) VALUES (:id_longueur,:nombre_longueur,:id_voie)";
+		MapSqlParameterSource vParams = new MapSqlParameterSource();
+		vParams.addValue("id_longueur", id, Types.INTEGER);
+		vParams.addValue("nombre_longueur", nblongueur, Types.INTEGER);
+		vParams.addValue("id_voie", id_voie, Types.INTEGER);
+		
+		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+			
+		try {
+			vJdbcTemplate.update(vSQL, vParams);
+			System.out.println("La longueur est bien ajoutée !");
+		} catch (DuplicateKeyException vEx) {
+			System.out.println("La longueur existe déjà !");			
+		}
 	}
 }
