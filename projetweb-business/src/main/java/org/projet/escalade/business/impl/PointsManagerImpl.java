4b7052cad00adb46917308afbc5fe03896580a16
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-23 15:07:29 +0200

    Mise à jour de PointsManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/PointsManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/PointsManagerImpl.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.projet.escalade.business.contract.PointsManager;
 import org.projet.escalade.model.Longueur;
 import org.projet.escalade.model.Points;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class PointsManagerImpl extends AbstractManager implements PointsManager {
 
@@ -18,5 +22,20 @@ public class PointsManagerImpl extends AbstractManager implements PointsManager 
 
 	public List<Points> getPointsByLongueur(Longueur vLongueur) {
 		return getDaoFactory().getPointsDao().getPointsByLongueur(vLongueur);
+	}
+
+	@Transactional(value = "txManagerSite")
+	public void AddPoints(final int id, final int nbpoints, final int id_longueur) {
+
+		TransactionTemplate vTransactionTemplate = new TransactionTemplate(getPlatformTransactionManager());
+
+		vTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+
+				getDaoFactory().getPointsDao().AddPoints(id, nbpoints, id_longueur);
+			};
+		});
 	}
 }
