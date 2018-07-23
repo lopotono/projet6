c0333d91fd3f9533f3f0a331ad62a173f39d3b74
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-23 15:09:02 +0200

    Mise à jour de ToposManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/ToposManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/ToposManagerImpl.java
@@ -5,24 +5,44 @@ import java.util.List;
 import org.projet.escalade.business.contract.ToposManager;
 import org.projet.escalade.model.Sites;
 import org.projet.escalade.model.Topos;
-import org.projet.escalade.model.User;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class ToposManagerImpl extends AbstractManager implements ToposManager {
 
-	
-    public List<Topos> getListTopos() {
+	public List<Topos> getListTopos() {
 		return getDaoFactory().getTopoDao().getTopos();
 	}
 
 	public Topos getTopo(int id) {
-		return getDaoFactory().getTopoDao().getTopos(id);
+		Topos vTopo = getDaoFactory().getTopoDao().getTopos(id);
+		List<Sites> vSite = getDaoFactory().getSitesDao().getSiteByTopos(vTopo);
+		vTopo.setSite(vSite);
+		return vTopo;
 	}
 
-	public List<Topos> getToposBySite(Sites vSite) {
-		return getDaoFactory().getTopoDao().getToposBySite(vSite);
+	public Topos getSearchTopo(int id, String name) {
+		return getDaoFactory().getTopoDao().getSearchTopo(id, name);
 	}
 
-	public List<Topos> getToposByUser(User vUser) {
-		return getDaoFactory().getTopoDao().getToposByUser(vUser);
-	}  	
+	public Topos getEmprunttopo(String name) {
+		return getDaoFactory().getTopoDao().getEmprunttopo(name);
+	}
+
+	@Transactional(value = "txManagerSite")
+	public void AjoutTopo(final String name, final Boolean dispo, final int id) {
+
+		TransactionTemplate vTransactionTemplate = new TransactionTemplate(getPlatformTransactionManager());
+
+		vTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+
+				getDaoFactory().getTopoDao().AjoutTopo(name, dispo, id);
+			};
+		});
+	}
 }
