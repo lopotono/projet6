1da962301f890f93876d7144058d6b031de076e8
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-23 15:06:50 +0200

    Mise à jour de LongueurManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/LongueurManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/LongueurManagerImpl.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.projet.escalade.business.contract.LongueurManager;
 import org.projet.escalade.model.Longueur;
 import org.projet.escalade.model.Voie;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class LongueurManagerImpl extends AbstractManager implements LongueurManager {
 
@@ -18,5 +22,20 @@ public class LongueurManagerImpl extends AbstractManager implements LongueurMana
 
 	public Longueur getLongueur(int id) {
 		return getDaoFactory().getLongueurDao().getLongueur(id);
+	}
+
+	@Transactional(value="txManagerSite")
+	public void AddLongueur(final int id, final int nblongueur, final int id_voie) {
+		
+		TransactionTemplate vTransactionTemplate = new TransactionTemplate(getPlatformTransactionManager());
+		
+		vTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				
+				getDaoFactory().getLongueurDao().AddLongueur(id, nblongueur, id_voie);
+			};
+		});			
 	}
 }
