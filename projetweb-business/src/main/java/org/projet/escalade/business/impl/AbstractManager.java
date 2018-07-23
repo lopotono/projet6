05b946e3f442c0a61b485a164226da32d6022d45
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-23 15:05:59 +0200

    Mise à jour de AbstractManager.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/AbstractManager.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/AbstractManager.java
@@ -1,6 +1,7 @@
 package org.projet.escalade.business.impl;
 
 import org.projet.escalade.consumer.contract.DaoFactory;
+import org.springframework.transaction.PlatformTransactionManager;
 
 public abstract class AbstractManager {
 
@@ -13,4 +14,16 @@ public abstract class AbstractManager {
     public void setDaoFactory(DaoFactory daoFactory) {
         this.daoFactory = daoFactory;
     }
+    
+    /*@Inject
+    @Named("txManagerSite")*/
+    private PlatformTransactionManager platformTransactionManager;
+    
+    public PlatformTransactionManager getPlatformTransactionManager() {
+		return platformTransactionManager;
+	}
+    
+    public void setPlatformTransactionManager(PlatformTransactionManager platformTransactionManager) {
+		this.platformTransactionManager = platformTransactionManager;
+	}
 }
