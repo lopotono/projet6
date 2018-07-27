00698a5c7a400b935f95eae8866bc36d6a27f605
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-27 17:12:50 +0200

    Mise à jour de AddSecteurAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/AddSecteurAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/AddSecteurAction.java
@@ -1,5 +1,10 @@
 package org.projet.escalade.webapp.action;
 
+import java.util.List;
+
+import org.projet.escalade.model.Secteur;
+import org.projet.escalade.model.Sites;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class AddSecteurAction extends AbstractAction {
@@ -11,6 +16,9 @@ public class AddSecteurAction extends AbstractAction {
 	
 	private String id_site;
 	private String name;
+	private List<Secteur> listSecteur;
+	private List<Sites> listSites;
+	private String idsites;
 	
 	public String getId_site() {
 		return id_site;
@@ -26,11 +34,27 @@ public class AddSecteurAction extends AbstractAction {
 		this.name = name;
 	}
 	
+	public List<Secteur> getListSecteur() {
+		return listSecteur;
+	}
+	public void setListSecteur(List<Secteur> listSecteur) {
+		this.listSecteur = listSecteur;
+	}
+	
+	public List<Sites> getListSites() {
+		return listSites;
+	}
+	public void setListSites(List<Sites> listSites) {
+		this.listSites = listSites;
+	}
+	
 	public String execute() {
 		
 		String result = ActionSupport.INPUT;
-		if (name != null && id_site != null) {
-			getManagerFactory().getSecteurManager().AddSecteur(name, Integer.valueOf(id_site));
+		listSites = getManagerFactory().getSitesManager().getListSites();
+		getManagerFactory().getSecteurManager().getSites(idsites);
+		if (name != null && idsites != null) {
+			getManagerFactory().getSecteurManager().AddSecteur(name, idsites);
 			result = ActionSupport.SUCCESS;
 		}
 		return result;		
