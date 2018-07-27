7a0e2cdd884a492ccc4103e80a5c616ca5e15cce
lopotono <30410435+lopotono@users.noreply.github.com>
2018-07-27 17:12:10 +0200

    Mise à jour de AddSiteAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/AddSiteAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/AddSiteAction.java
@@ -1,5 +1,10 @@
 package org.projet.escalade.webapp.action;
 
+import java.util.List;
+
+import org.projet.escalade.model.Sites;
+import org.projet.escalade.model.Topos;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class AddSiteAction extends AbstractAction {
@@ -12,6 +17,9 @@ public class AddSiteAction extends AbstractAction {
 	private String id_topo;
 	private String name;
 	private String description;
+	private Sites site;
+	private List<Topos> listTopos;
+	private String idtopo;
 
 	public String getName() {
 		return name;
@@ -36,12 +44,38 @@ public class AddSiteAction extends AbstractAction {
 	public void setId_topo(String id_topo) {
 		this.id_topo = id_topo;
 	}
+	
+	public Sites getSite() {
+		return site;
+	}
+
+	public void setSite(Sites site) {
+		this.site = site;
+	}
+	
+	public List<Topos> getListTopos() {
+		return listTopos;
+	}
+
+	public void setListTopos(List<Topos> listTopos) {
+		this.listTopos = listTopos;
+	}
+	
+	public String getIdtopo() {
+		return idtopo;
+	}
+	
+	public void setIdtopo(String idtopo) {
+		this.idtopo = idtopo;
+	}
 
 	public String execute() {
 
 		String result = ActionSupport.INPUT;
-		if (name != null && description != null && id_topo != null) {
-			getManagerFactory().getSitesManager().AddSite(name, Integer.valueOf(id_topo) , description);
+		listTopos = getManagerFactory().getToposManager().getListTopos();
+		getManagerFactory().getSitesManager().getTopos(idtopo);
+		if (name != null && description != null && idtopo != null) {
+			getManagerFactory().getSitesManager().AddSite(name, idtopo, description);
 			result = ActionSupport.SUCCESS;
 		}
 		return result;	
