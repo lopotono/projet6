a97c934c22f63f88ee289951ad1a9603674953da
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-01 16:58:40 +0200

    Mise à jour de EmpruntAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/EmpruntAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/EmpruntAction.java
@@ -2,12 +2,17 @@ package org.projet.escalade.webapp.action;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.struts2.interceptor.SessionAware;
 import org.projet.escalade.model.Emprunttopo;
 import org.projet.escalade.model.Topos;
+import org.projet.escalade.model.User;
+import org.projet.escalade.model.exception.NotFoundException;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class EmpruntAction extends AbstractAction {
+public class EmpruntAction extends AbstractAction implements SessionAware {
 
 	/**
 	 * 
@@ -17,6 +22,13 @@ public class EmpruntAction extends AbstractAction {
 	private List<Topos> listTopos;
 	private Emprunttopo emprunttopo;
 	private String topoid;
+	private String name;
+	private boolean empruntencours;
+	private Date datedebut;
+	private Date datefin;
+	private String id_topo;
+	private String id_user;
+	private Map<String, Object> session;
 
 	public List<Topos> getListTopos() {
 		return listTopos;
@@ -41,25 +53,85 @@ public class EmpruntAction extends AbstractAction {
 	public void setTopoid(String topoid) {
 		this.topoid = topoid;
 	}
+	
+	public boolean isEmpruntencours() {
+		return empruntencours;
+	}
+
+	public void setEmpruntencours(boolean empruntencours) {
+		this.empruntencours = empruntencours;
+	}
+
+	public Date getDatedebut() {
+		return datedebut;
+	}
+
+	public void setDatedebut(Date datedebut) {
+		this.datedebut = datedebut;
+	}
+
+	public Date getDatefin() {
+		return datefin;
+	}
+
+	public void setDatefin(Date datefin) {
+		this.datefin = datefin;
+	}
+	
+	public String getId_topo() {
+		return id_topo;
+	}
+	
+	public void setId_topo(String id_topo) {
+		this.id_topo = id_topo;
+	}
+	
+	public String getId_user() {
+		return id_user;
+	}
+	
+	public void setId_user(String id_user) {
+		this.id_user = id_user;
+	}
 
 	public String execute() {
 
 		String vResult = ActionSupport.INPUT;
-		//Récupérer la liste des topos disponibles
+		// Récupérer la liste des topos disponibles
 		listTopos = getManagerFactory().getToposManager().getListTopos();
+		emprunttopo = new Emprunttopo();
+		// Récupérer le nom de l'utilisateur
+		User vUser = getManagerFactory().getUserManager().getUser(name);
+		if (vUser == null) {
+			try {
+				throw new NotFoundException();
+			} catch (NotFoundException e) {
+				this.addActionError("Erreur");
+			}
+		}
+		this.session.get("user");
+		
 		// Vérifier si le topo existe
 		if (this.topoid != null) {
-			try {
-				
+			try {				
 				this.emprunttopo = new Emprunttopo();
-				this.emprunttopo.setDateEmprunt(new Date());
+				this.emprunttopo.setDatedebut(datedebut);
+				this.emprunttopo.setDatefin(datefin);
 				this.emprunttopo.setTopos(getManagerFactory().getToposManager().getTopo(topoid));
-				//sauvegarder l'emprunt
+				
 				vResult = ActionSupport.SUCCESS;
 			} catch (Exception e) {
 				this.addActionError("Erreur");
-			}		
+			}
+		}
+		//sauvegarder l'emprunt
+		if (datedebut != null && datefin != null && id_topo != null && id_user != null) {
+			getManagerFactory().getEmpruntManager().AddEmprunt(datedebut, datefin, Integer.valueOf(id_topo), Integer.valueOf(id_user));
 		}
 		return vResult;
+	}
+
+	public void setSession(Map<String, Object> pSession) {
+		this.session = pSession;		
 	}	
 }
