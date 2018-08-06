f127e1d47855096857b8d598ea210b21b89abf93
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-06 14:55:53 +0200

    Mise à jour de EmpruntDAO.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/EmpruntDAO.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/EmpruntDAO.java
@@ -4,10 +4,13 @@ import java.util.Date;
 import java.util.List;
 
 import org.projet.escalade.model.Emprunttopo;
+import org.projet.escalade.model.User;
 
 public interface EmpruntDAO {
 	
-	List<Emprunttopo> getEmprunttopo();
+	List<Emprunttopo> getListEmprunttopo();
 
 	void AddEmprunt(Date datedebut, Date datefin, int id_topo, int id_user);
+
+	List<Emprunttopo> getEmpruntByUser(User vUser);
 }
