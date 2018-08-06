53f6618ed61c8716bb392dcc073e7fed23a7ab31
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-06 14:53:48 +0200

    Mise à jour de EmpruntManager.java

--- projetweb-business/src/main/java/org/projet/escalade/business/contract/EmpruntManager.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/contract/EmpruntManager.java
@@ -1,8 +1,16 @@
 package org.projet.escalade.business.contract;
 
 import java.util.Date;
+import java.util.List;
+
+import org.projet.escalade.model.Emprunttopo;
+import org.projet.escalade.model.User;
 
 public interface EmpruntManager {
 	
 	void AddEmprunt(Date datedebut, Date datefin, int id_topo, int id_user);
+	
+	List<Emprunttopo> getEmpruntByUser(User vUser);
+	
+	List<Emprunttopo> getListEmprunttopo();
 }
