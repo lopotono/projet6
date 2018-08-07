8f4684dbac7b58abfa8f29c7129fb7aa0acd374e
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-07 10:27:45 +0200

    Mise à jour de ToposManager.java

--- projetweb-business/src/main/java/org/projet/escalade/business/contract/ToposManager.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/contract/ToposManager.java
@@ -3,10 +3,13 @@ package org.projet.escalade.business.contract;
 import java.util.List;
 
 import org.projet.escalade.model.Topos;
+import org.projet.escalade.model.User;
 
 public interface ToposManager {
 
 	List<Topos> getListTopos();
+	
+	List<Topos> getToposdisponible(boolean topodispo);
 
 	Topos getTopo(int id);
 	
@@ -15,10 +18,14 @@ public interface ToposManager {
 	void AjoutTopo(String name, Boolean dispo, String iduser);
 
 	Topos getEmprunttopo(String name);
+	
+	Topos getToponame(String toponame);
 
 	Topos getTopo(String topoid);
 	
 	Topos getListDisponible(Boolean listdispo);
 
 	Topos getUser(String iduser);
+	
+	List<Topos> getEmpruntByUser(User vUser);
 }
