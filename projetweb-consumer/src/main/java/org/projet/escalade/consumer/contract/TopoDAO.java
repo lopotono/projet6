ce86ae71217a9ef1ae6b22ac15d3e24577185f60
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-07 10:29:49 +0200

    Mise à jour de TopoDAO.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/TopoDAO.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/contract/TopoDAO.java
@@ -7,12 +7,14 @@ import org.projet.escalade.model.Topos;
 public interface TopoDAO {
 
 	List<Topos> getTopos();
+	
+	List<Topos> getToposdisponible(boolean topodispo);
 
 	Topos getTopos(int id);
 
 	Topos getSearchTopo(int id, String name);
 
-	void AjoutTopo(String name, Boolean dispo, String iduser);
-
 	Topos getEmprunttopo(String name);
+
+	void AjoutTopo(String name, Boolean dispo, String iduser);
 }
