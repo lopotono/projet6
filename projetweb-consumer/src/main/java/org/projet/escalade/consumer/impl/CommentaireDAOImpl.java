6d250e9f0c199e1e4516919ee4b170db0523e4fe
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-10 14:56:32 +0200

    Ajout de CommentaireDAOImpl.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/CommentaireDAOImpl.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/CommentaireDAOImpl.java
@@ -1,30 +1,54 @@
 package org.projet.escalade.consumer.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.Types;
 import java.util.List;
 
 import org.projet.escalade.consumer.contract.CommentaireDAO;
+import org.projet.escalade.consumer.impl.rawmapper.CommentaireRawMapper;
 import org.projet.escalade.model.Commentaire;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.projet.escalade.model.Sites;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class CommentaireDAOImpl extends AbstractDAO implements CommentaireDAO {
 
-	public List<Commentaire> getCommentaire() {
-		String vSQL = "SELECT * FROM commentaire";
+	public List<Commentaire> getCommentaireBySite(Sites vSite) {
 		
-		JdbcTemplate vJdbcTemplate = new JdbcTemplate(getDataSource());
+		String vSQL = "SELECT * FROM commentaire WHERE id_site_escalade=" + vSite.getSitesid();
 		
-		RowMapper<Commentaire> vRowMapper = new RowMapper<Commentaire>() {
-			public Commentaire mapRow(ResultSet pRS, int pRowNum) throws SQLException {
-				Commentaire vCommentaire = new Commentaire();
-				vCommentaire.setCommentairename(pRS.getString("nom_commentaire"));
-				return vCommentaire;
-			}
-		};
-		List<Commentaire> vListCommentaire = vJdbcTemplate.query(vSQL, vRowMapper);
+		CommentaireRawMapper vRowMapper = new CommentaireRawMapper();
+		
+		List<Commentaire> vListCommentaire = getJdbcTemplate().query(vSQL, vRowMapper);
 		
 		return vListCommentaire;
+	}
+
+	public List<Commentaire> getListCommentaire() {
+		
+		String vSQL = "SELECT * FROM commentaire ORDER BY nom_commentaire";
+		
+		CommentaireRawMapper vRowMapper = new CommentaireRawMapper();
+		
+		List<Commentaire> vListCommentaire = getJdbcTemplate().query(vSQL, vRowMapper);
+		
+		return vListCommentaire;
+	}
+
+	public void AddCommentaire(String name, String contain, String idsite) {
+		
+		String vSQL = "INSERT INTO commentaire (nom_commentaire, contenu, id_site_escalade) VALUES (:nom_commentaire,:contenu,:id_site_escalade)";
+		MapSqlParameterSource vParams = new MapSqlParameterSource();
+		vParams.addValue("nom_commentaire", name, Types.VARCHAR);
+		vParams.addValue("contenu", contain, Types.INTEGER);
+		vParams.addValue("id_site_escalade", idsite, Types.VARCHAR);
+		
+		NamedParameterJdbcTemplate vJdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+			
+		try {
+			vJdbcTemplate.update(vSQL, vParams);
+		} catch (DuplicateKeyException vEx) {
+		
+		}		
 	}
 }
