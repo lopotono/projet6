d0c2dbedda520a013ae61da2cc3a4e84ecf87239
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-10 14:49:21 +0200

    Ajout de CommentaireManager.java

--- projetweb-business/src/main/java/org/projet/escalade/business/contract/CommentaireManager.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/contract/CommentaireManager.java
@@ -3,22 +3,15 @@ package org.projet.escalade.business.contract;
 import java.util.List;
 
 import org.projet.escalade.model.Commentaire;
-import org.projet.escalade.model.Message;
-import org.projet.escalade.model.exception.FunctionalException;
-import org.projet.escalade.model.exception.NotFoundException;
-import org.projet.escalade.model.exception.TechnicalException;
+import org.projet.escalade.model.Sites;
 
 public interface CommentaireManager {
+	
+	List<Commentaire> getCommentaireBySite(Sites vSite);
 
-	public List<Commentaire> getCommentaire();
+	List<Commentaire> getListCommentaire();
+	
+	void AddCommentaire(String name, String contain, String idsite);
 
-	Commentaire getCommentaire(String pName) throws NotFoundException;
-
-	void addCommentaire(Commentaire pCommentaire) throws FunctionalException, TechnicalException;
-
-	List<Message> getListNewMessage(Commentaire pCommentaire, Integer pLastMessageindex)
-			throws NotFoundException, TechnicalException;
-
-	void addMessage(Commentaire pCommentaire, Message pMessage)
-			throws FunctionalException, NotFoundException, TechnicalException;
+	Commentaire getSites(String idsite);
 }
