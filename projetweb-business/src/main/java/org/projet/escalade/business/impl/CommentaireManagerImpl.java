617cf08f6d4a02e7cb708a3846847ee203480ac3
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-10 14:50:32 +0200

    Mise à jour de CommentaireManagerImpl.java

--- projetweb-business/src/main/java/org/projet/escalade/business/impl/CommentaireManagerImpl.java
+++ projetweb-business/src/main/java/org/projet/escalade/business/impl/CommentaireManagerImpl.java
@@ -4,10 +4,39 @@ import java.util.List;
 
 import org.projet.escalade.business.contract.CommentaireManager;
 import org.projet.escalade.model.Commentaire;
+import org.projet.escalade.model.Sites;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class CommentaireManagerImpl extends AbstractManager implements CommentaireManager {
 
-	public List<Commentaire> getCommentaire() {
-		return getDaoFactory().getCommentaireDao().getCommentaire();
+	public List<Commentaire> getCommentaireBySite(Sites vSite) {
+		return getDaoFactory().getCommentaireDao().getCommentaireBySite(vSite);
+	}
+
+	public List<Commentaire> getListCommentaire() {
+		return getDaoFactory().getCommentaireDao().getListCommentaire();
+	}
+
+	@Transactional(value="txManagerSite")
+	public void AddCommentaire(final String name, final String contain, final String idsite) {
+		
+		TransactionTemplate vTransactionTemplate = new TransactionTemplate(getPlatformTransactionManager());
+		
+		vTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				
+				getDaoFactory().getCommentaireDao().AddCommentaire(name, contain, idsite);
+			};
+		});
+		
+	}
+
+	public Commentaire getSites(String idsite) {
+		return null;
 	}
 }
