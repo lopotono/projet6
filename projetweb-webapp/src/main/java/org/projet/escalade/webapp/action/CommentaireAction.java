d072e500fe4baced391a0346b44c6486a5a4112c
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-11 13:51:21 +0200

    Ajout de CommentaireAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/CommentaireAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/CommentaireAction.java
@@ -3,6 +3,9 @@ package org.projet.escalade.webapp.action;
 import java.util.List;
 
 import org.projet.escalade.model.Commentaire;
+import org.projet.escalade.model.Sites;
+
+import com.opensymphony.xwork2.ActionSupport;
 
 public class CommentaireAction extends AbstractAction {
 
@@ -11,27 +14,43 @@ public class CommentaireAction extends AbstractAction {
 	 */
 	private static final long serialVersionUID = -2111586829219794628L;
 	
-	private List<Commentaire> listCommentaire;
+	private int id;
 	private Commentaire commentaire;
-
-	public List<Commentaire> getListCommentaire() {
-		return listCommentaire;
+	private String idsite;
+	private List<Sites> listSites;
+	
+	public int getId() {
+		return id;
 	}
-
-	public void setListCommentaire(List<Commentaire> listCommentaire) {
-		this.listCommentaire = listCommentaire;
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 	public Commentaire getCommentaire() {
 		return commentaire;
 	}
-
 	public void setCommentaire(Commentaire commentaire) {
 		this.commentaire = commentaire;
 	}
 	
-	public String doCommentaire() {
-		System.out.println(this.getClass()+"doCommentaire()");
-		return SUCCESS;
+	public String getIdsite() {
+		return idsite;
+	}
+	public void setIdsite(String idsite) {
+		this.idsite = idsite;
+	}
+	
+	public List<Sites> getListSites() {
+		return listSites;
+	}
+	public void setListSites(List<Sites> listSites) {
+		this.listSites = listSites;
+	}
+	
+	public String execute() {
+		setListSites(getManagerFactory().getSitesManager().getListSites());
+		commentaire = getManagerFactory().getCommentaireManager().getCommentaire(id);
+		this.commentaire.setCommentaire(getManagerFactory().getCommentaireManager().getSites(idsite));
+		return ActionSupport.SUCCESS;
 	}
 }
