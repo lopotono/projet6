b8cd0ca89868c651d676e44882229cd795046db5
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-13 11:27:56 +0200

    Mise à jour de EmpruntAction.java

--- projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/EmpruntAction.java
+++ projetweb-webapp/src/main/java/org/projet/escalade/webapp/action/EmpruntAction.java
@@ -21,14 +21,10 @@ public class EmpruntAction extends AbstractAction implements SessionAware {
 	private List<Topos> listTopos;
 	private Emprunttopo emprunttopo;
 	private String topoid;
-	private String name;
-	private String toposname;
-	private boolean empruntencours;
 	private Date datedebut;
 	private Date datefin;
-	private String id_topo;
-	private String id_user;
 	private boolean topodispo;
+	private String name;
 	private Map<String, Object> session;
 
 	public List<Topos> getListTopos() {
@@ -54,14 +50,6 @@ public class EmpruntAction extends AbstractAction implements SessionAware {
 	public void setTopoid(String topoid) {
 		this.topoid = topoid;
 	}
-	
-	public boolean isEmpruntencours() {
-		return empruntencours;
-	}
-
-	public void setEmpruntencours(boolean empruntencours) {
-		this.empruntencours = empruntencours;
-	}
 
 	public Date getDatedebut() {
 		return datedebut;
@@ -79,22 +67,6 @@ public class EmpruntAction extends AbstractAction implements SessionAware {
 		this.datefin = datefin;
 	}
 	
-	public String getId_topo() {
-		return id_topo;
-	}
-	
-	public void setId_topo(String id_topo) {
-		this.id_topo = id_topo;
-	}
-	
-	public String getId_user() {
-		return id_user;
-	}
-	
-	public void setId_user(String id_user) {
-		this.id_user = id_user;
-	}
-		
 	public String getName() {
 		return name;
 	}
@@ -102,40 +74,28 @@ public class EmpruntAction extends AbstractAction implements SessionAware {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getToposname() {
-		return toposname;
-	}
 
-	public void setToposname(String toposname) {
-		this.toposname = toposname;
-	}
-		
 	public String execute() {
 
 		String vResult = ActionSupport.INPUT;
 		// Récupérer la liste des topos disponibles
-		//listTopos = getManagerFactory().getToposManager().getListTopos();
 		listTopos = getManagerFactory().getToposManager().getToposdisponible(topodispo);
 		emprunttopo = new Emprunttopo();
 		// Récupérer le nom de l'utilisateur
-		User vUser = (User)this.session.get("user");
+		User vUser = (User) this.session.get("user");
 		this.setName(vUser.getName());
-		
-		//Topos topos = getManagerFactory().getToposManager().getTopo(topoid); 
-		//this.setToposname(topos.getToposname());
 
 		// Calcul de la période de prêt
-		
-					
+
 		// Vérifier si le topo existe
 		if (this.topoid != null) {
-			try {				
+			try {
 				this.emprunttopo = new Emprunttopo();
 				this.emprunttopo.setDatedebut(datedebut);
 				this.emprunttopo.setDatefin(datefin);
 				this.emprunttopo.setTopos(getManagerFactory().getToposManager().getTopo(topoid));
-			
+				//this.emprunttopo.setName(vUser);
+				getManagerFactory().getEmpruntManager().SaveEmprunt(datedebut, datefin, vUser.getId().toString(), topoid);
 				vResult = ActionSupport.SUCCESS;
 			} catch (Exception e) {
 				this.addActionError("Erreur");
@@ -145,6 +105,6 @@ public class EmpruntAction extends AbstractAction implements SessionAware {
 	}
 
 	public void setSession(Map<String, Object> pSession) {
-		this.session = pSession;		
+		this.session = pSession;
 	}	
 }
