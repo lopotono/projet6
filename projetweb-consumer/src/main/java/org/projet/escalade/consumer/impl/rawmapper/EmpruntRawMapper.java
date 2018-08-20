e92862a43caa45507a215b54efe05dbed4f809e3
lopotono <30410435+lopotono@users.noreply.github.com>
2018-08-20 17:42:30 +0200

    Mise à jour de EmpruntRawMapper.java

--- projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/rawmapper/EmpruntRawMapper.java
+++ projetweb-consumer/src/main/java/org/projet/escalade/consumer/impl/rawmapper/EmpruntRawMapper.java
@@ -2,6 +2,8 @@ package org.projet.escalade.consumer.impl.rawmapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
+
 import org.springframework.jdbc.core.RowMapper;
 import org.projet.escalade.model.Emprunttopo;
 
@@ -10,8 +12,13 @@ public class EmpruntRawMapper implements RowMapper<Emprunttopo> {
 	public Emprunttopo mapRow(ResultSet pRS, int pRowNum) throws SQLException {
 		Emprunttopo vEmprunttopo = new Emprunttopo();
 		vEmprunttopo.setIdemprunt(pRS.getInt("id_emprunt_topo"));
-		vEmprunttopo.setDatedebut(pRS.getDate("date_debut_emprunt"));
-		vEmprunttopo.setDatefin(pRS.getDate("date_fin_emprunt"));
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(pRS.getDate("date_debut_emprunt"));
+		vEmprunttopo.setDatedebut(cal);
+		Calendar cal1 = Calendar.getInstance();
+		cal1.setTime(pRS.getDate("date_fin_emprunt"));
+		vEmprunttopo.setDatefin(cal1);
+		vEmprunttopo.setId_topo(pRS.getString("id_topo"));
 		return vEmprunttopo;
 	}
 }
